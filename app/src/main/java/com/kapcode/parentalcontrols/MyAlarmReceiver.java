ef78e0f2855ee1a17f9f9fbe62eb6f2e20217167
Kyle <eatlinux@gmail.com>
2023-11-14 18:18:58 -0500

    Created Example WatchDog Using AlarmManager, Works Great!

---
+++ app/src/main/java/com/kapcode/parentalcontrols/MyAlarmReceiver.java
@@ -0,0 +1,26 @@
+package com.kapcode.parentalcontrols;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyAlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm went off", Toast.LENGTH_SHORT).show();
+        //user didn't stop it, and it is not running, android must have stopped it
+        if(!ExampleService.serviceStoppedByUser.get() && !ExampleService.serviceIsRunning.get()){
+            //Restart It Here
+            System.out.println("Restart Code Here TODO");
+            MainActivity.startService();
+        }else{
+            System.out.println("All Good");
+        }
+
+
+        //start watchdog again.
+        MainActivity.watchDog(MainActivity.alarmManager,MainActivity.context);
+
+    }
+}
